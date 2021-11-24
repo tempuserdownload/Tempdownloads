bfb30ede05689681673e3a809c62e1086caea148
tempuserdownload <94956624+tempuserdownload@users.noreply.github.com>
2021-11-24 10:16:27 +0100

    Add files via upload

--- Bash/download.sh
+++ Bash/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-curl https://raw.githubusercontent.com/Knuffelbeer4game/ProjectFurno/main/Bash/lamp.sh?token=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 -o lamp.sh
-curl https://raw.githubusercontent.com/Knuffelbeer4game/ProjectFurno/main/Bash/nextcloud.sh?token=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 nextcloud.sh
+curl https://raw.githubusercontent.com/tempuserdownload/Tempdownloads/main/Bash/lamp.sh -o lamp.sh
+curl https://raw.githubusercontent.com/tempuserdownload/Tempdownloads/main/Bash/nextcloud.sh nextcloud.sh
 sudo chmod +x nextcloud.sh
 sudo chmod +x lamp.sh
 ./lamp.sh
